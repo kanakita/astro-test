8207a3b4da3d73645bf9a3a079fde5aae22edd63
redamoon <yuuki60824@gmail.com>
2023-06-19 18:34:21 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,8 +10,22 @@ export default defineConfig({
     build: {
       rollupOptions: {
         output: {
-          entryFileNames: 'assets/js/app.js',
-          assetFileNames: 'assets/css/style[extname]',
+          entryFileNames: 'assets/js/[name].js',
+          assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.')[1];
+          //Webフォントファイルの振り分け
+          if (/ttf|otf|eot|woff|woff2/i.test(extType)) {
+            extType = 'fonts';
+          }
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'images';
+          }
+          //ビルド時のCSS名を明記してコントロールする
+          if(extType === 'css') {
+            return `assets/css/style.css`;
+          }
+          return `assets/${extType}/[name][extname]`;
+        },
         }
       }
     }
